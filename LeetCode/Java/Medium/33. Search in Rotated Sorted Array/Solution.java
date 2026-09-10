b0837011dd37c3b4bb78fcd4c28a5304f4d7e1b3
Prashant Thorat <prashant.workemail0@gmail.com>
2026-09-10 18:07:59 +0530

    Add Medium Java Solution for 33. Search in Rotated Sorted Array from LeetCode

--- LeetCode/Java/Medium/33. Search in Rotated Sorted Array/Solution.java
+++ LeetCode/Java/Medium/33. Search in Rotated Sorted Array/Solution.java
@@ -8,15 +8,24 @@ class Solution {
             if(nums[mid]==target){
                 return mid;
             }
-            else if(nums[s]<nums[mid] && (nums[s]<= target && target<=nums[mid])) {
-                e = mid;
+            else if(nums[s]<nums[mid] ) {
+                if((nums[s]<= target && target<=nums[mid])){
+                    e = mid;
+                }
+                else{
+                    s = mid+1;
+                }
+                
             }
-            else if(nums[mid] < nums[e] && (nums[mid]<= target && target<=nums[e])){
-                s = mid;
+            else if(nums[mid] < nums[e] ){
+                if(nums[mid]<= target && target<=nums[e]){
+                    s = mid;
+                }
+                else{
+                    e = mid-1;
+                }
             }
-            else {
-                s = mid +1;
-            }
+            
         }
         return -1;
     }

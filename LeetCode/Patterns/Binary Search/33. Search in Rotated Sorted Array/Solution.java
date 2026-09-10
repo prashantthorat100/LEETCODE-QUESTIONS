855c74cc6c6d80392b80568014fff928f79c5dda
Prashant Thorat <prashant.workemail0@gmail.com>
2026-09-10 18:11:11 +0530

    Add Medium Java Solution for 33. Search in Rotated Sorted Array from LeetCode

--- LeetCode/Patterns/Binary Search/33. Search in Rotated Sorted Array/Solution.java
+++ LeetCode/Patterns/Binary Search/33. Search in Rotated Sorted Array/Solution.java
@@ -1,32 +1,36 @@
 class Solution {
     public int search(int[] nums, int target) {
-        int s = 0 ;
-        int e = nums.length-1;
+        int s = 0;
+        int e = nums.length - 1;
 
-        while(s<=e){
-            int mid = s + (e-s)/2;
-            if(nums[mid]==target){
+        while (s <= e) {
+            int mid = s + (e - s) / 2;
+
+            if (nums[mid] == target) {
                 return mid;
             }
-            else if(nums[s]<nums[mid] ) {
-                if((nums[s]<= target && target<=nums[mid])){
-                    e = mid;
+
+            // Left half is sorted
+            if (nums[s] <= nums[mid]) {
+
+                if (nums[s] <= target && target < nums[mid]) {
+                    e = mid - 1;
+                } else {
+                    s = mid + 1;
                 }
-                else{
-                    s = mid+1;
-                }
-                
+
             }
-            else if(nums[mid] < nums[e] ){
-                if(nums[mid]<= target && target<=nums[e]){
-                    s = mid;
-                }
-                else{
-                    e = mid-1;
+            // Right half is sorted
+            else {
+
+                if (nums[mid] < target && target <= nums[e]) {
+                    s = mid + 1;
+                } else {
+                    e = mid - 1;
                 }
             }
-            
         }
+
         return -1;
     }
 }

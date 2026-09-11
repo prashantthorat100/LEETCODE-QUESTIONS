cd01b8b33c03b8e072e192bc689d75398135e7d1
Prashant Thorat <prashant.workemail0@gmail.com>
2026-09-11 13:53:02 +0530

    Add Medium Java Solution for 74. Search a 2D Matrix from LeetCode

--- LeetCode/Java/Medium/74. Search a 2D Matrix/Solution.java
+++ LeetCode/Java/Medium/74. Search a 2D Matrix/Solution.java
@@ -1,25 +1,28 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-        
-        int s = 0;
-        int e = (matrix.length)*(matrix[0].length)-1;
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int low = 0;
+        int high = m*n -1;
 
-        while(s<=e){
-            int mid = s + (e-s)/2;
-            int row = mid / matrix[0].length;
-            int col = mid % matrix[0].length;
+        while(low<=high){
+            int mid = low + (high-low)/2;
+            int row = mid/n;
+            int col = mid%n; 
 
             if(matrix[row][col]==target){
                 return true;
-                }
-            else if(matrix[row][col] < target){ 
-                s = mid+1; 
-                }
-            else if(target<matrix[row][col]){ 
-                e = mid -1;
-                }
+            }
+            else if( matrix[row][col]<target){
+                low = mid + 1;
+            }
+            else if( matrix[row][col]>target){
+                high = mid - 1;
+            }
+
             
         }
-        return false;
+        
+    return false;
     }
 }
--- LeetCode/Patterns/Binary Search/74. Search a 2D Matrix/README.md
+++ LeetCode/Patterns/Binary Search/74. Search a 2D Matrix/README.md
@@ -8,8 +8,8 @@
 Array, Binary Search, Matrix
 
 ### 🚀 Performance
-- **Runtime:** 0 ms
-- **Memory:** 44 MB
+- **Runtime:** N/A
+- **Memory:** N/A
 
 ---
 

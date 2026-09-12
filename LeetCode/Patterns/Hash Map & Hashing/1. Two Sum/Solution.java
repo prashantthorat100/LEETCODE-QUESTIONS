29d65699e2cd00d0a3560b6dea68f56aafea18ce
Prashant Thorat <prashant.workemail0@gmail.com>
2026-09-12 21:50:24 +0530

    Add Easy Java Solution for 1. Two Sum from LeetCode

--- LeetCode/Patterns/Hash Map & Hashing/1. Two Sum/Solution.java
+++ LeetCode/Patterns/Hash Map & Hashing/1. Two Sum/Solution.java
@@ -4,7 +4,7 @@ class Solution {
         for(int i=0;i<nums.length;i++){
             for(int j=i+1;j<nums.length;j++){
                 if(nums[i]+nums[j]==target){
-                    return ans[]{i}{j};
+                    return new int[]{i, j};
                 }
             }
         }

c3c0d2150eb9ebadc29f590fecd72199ca741e9f
Prashant Thorat <prashant.workemail0@gmail.com>
2026-09-20 13:51:39 +0530

    Add Medium Java Solution for 204. Count Primes from LeetCode

--- LeetCode/Java/Medium/204. Count Primes/Solution.java
+++ LeetCode/Java/Medium/204. Count Primes/Solution.java
@@ -1,18 +1,30 @@
 class Solution {
     public int countPrimes(int n) {
-        int count = 0;
 
-        for (int num = 2; num < n; num++) {
-            boolean isPrime = true;
+        if (n <= 2) {
+            return 0;
+        }
 
-            for (int i = 2; i <= Math.sqrt(num); i++) {
-                if (num % i == 0) {
-                    isPrime = false;
-                    break;
+        boolean[] isPrime = new boolean[n];
+
+        for (int i = 2; i < n; i++) {
+            isPrime[i] = true;
+        }
+
+        for (int i = 2; i * i < n; i++) {
+
+            if (isPrime[i]) {
+
+                for (int j = i * i; j < n; j += i) {
+                    isPrime[j] = false;
                 }
             }
+        }
 
-            if (isPrime) {
+        int count = 0;
+
+        for (int i = 2; i < n; i++) {
+            if (isPrime[i]) {
                 count++;
             }
         }

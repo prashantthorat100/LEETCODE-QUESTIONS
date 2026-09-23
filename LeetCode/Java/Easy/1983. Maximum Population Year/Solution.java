a8321ad2b505026aaa8f570e43b351caa8509a4b
Prashant Thorat <prashant.workemail0@gmail.com>
2026-09-23 19:24:00 +0530

    Add Easy Java Solution for 1983. Maximum Population Year from LeetCode

---
+++ LeetCode/Java/Easy/1983. Maximum Population Year/Solution.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int maximumPopulation(int[][] logs) {
+        int year[] = new int[logs.length];
+        int freq[] = new int[logs.length];
+
+        for(int i=0;i<logs.length;i++){
+            year[i] = logs[i][0];
+            for(int j=0;j<logs.length;j++){
+                if(logs[j][0]<=year[i] && year[i]<logs[j][1]){
+                freq[i]++;
+                }
+            }
+        }
+        int maxidx = 0;
+        int max = freq[0];
+        for(int i =1;i<freq.length;i++){
+            if(max<freq[i]){
+                max = freq[i];
+                maxidx = i;
+            }
+        }
+        
+        return year[maxidx];
+
+    }
+}
